7e0717a285e87ace734cc5b3fed54a28c3da88db
Allison Gardiner <allie.gardiner@gmail.com>
2017-03-29 18:01:06 +0000

    deleted file names using hello since we're using polls now

--- hello/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-import time
-
-# Create your views here.
-def index(request):
-    return HttpResponse("Hello, world at " + time.strftime("%c"))
