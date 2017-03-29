adf32772bde91956fb0d7c2db5a561905355872d
Allison Gardiner <allie.gardiner@gmail.com>
2017-03-29 17:57:31 +0000

    set up database schema

--- polls/models.py
+++ polls/models.py
@@ -3,4 +3,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
-
+#class inherits from models.Model , the class is django's version of a database table
+class PageCount(models.Model):
+        page = models.URLField(default='defaultURL')
+        count = models.IntegerField()
