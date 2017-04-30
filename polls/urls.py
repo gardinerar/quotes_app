4059ad99360040b180de3f53bd3ca3e98d47998a
Allison Gardiner <allie.gardiner@gmail.com>
2017-04-30 05:31:47 +0000

    Removed some unnecessary comments

--- polls/urls.py
+++ polls/urls.py
@@ -6,7 +6,4 @@ urlpatterns = [
     #The dollar sign matches the end of the string
     url(r'^$', views.detail, name='detail'),
     url(r'^data/', views.index, name='index'),
-    # url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name = "detail"),
-    # url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name = "results"),
-    # url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name = "vote"),
 ]
